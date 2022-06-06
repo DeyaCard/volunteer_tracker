9fefa69028e5e7734aefa15913593d0d58c83858
Deya Card <thedeyacard@gmail.com>
2022-06-06 14:30:49 -0700

    dry up project.rb and volunteer.rb code

--- lib/project.rb
+++ lib/project.rb
@@ -13,18 +13,22 @@ class Project
     returned_projects.each() do |project|
       title = project.fetch("title")
       id = project.fetch("id").to_i
-      projects.push(project.new({:title => title, :id => id}))
+      projects.push(Project.new({:title => title, :id => id}))
     end
     projects
   end
 
   def save
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-    @id = result.first().fetch("id").to_i
+    @id = result.first.fetch("id").to_i
   end
 
   def ==(project_to_compare)
-    self.title() == project_to_compare.title()
+    if project_to_compare != nil
+      (self.title == project_to_compare.title)
+    else
+      false
+    end
   end
 
   def self.clear
